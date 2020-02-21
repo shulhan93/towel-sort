03c59a775ad5ac6dddee293378f45e61cefe1e9a
Shulhan Andrei <shulhan.andrei@gmail.com>
2020-02-21 19:01:03 +0300

    feat: finished task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
+module.exports = function towelSort(matrix = []) {
+    if (matrix.length == 0) {
+        return []
+    };
 
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
+    let mas = matrix.reduce((sum, current, i) => {
+        if (i % 2 != 0) {
+            let sort = current.sort((a, b) => b - a);
+            sum.push(sort)
+        } else {
+            sum.push(current)
+        }
+        return sum
+    }, [])
+     
+    return mas.join(',').split(',').map(el => +el)
 }
